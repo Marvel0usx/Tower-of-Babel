365e040f70a5388a035065873bcd7cf3905abc68
Wang Haoyan <wangh278@teach.cs.toronto.edu>
2019-11-20 22:51:41 -0500

    Add general_counter.v

---
+++ general_counter.v
@@ -0,0 +1,35 @@
+/* General Counter
+ * -----------------
+ *
+ * This module is used to produce a delay of clock by a given count. It 
+ * provides synchornized signal for the VGA adapter and other use in the
+ * game.
+ *
+ */
+
+module general_counter(
+	input clk,
+	input resetn,
+	input enable,
+	input [25:0] delay,
+	output q
+	);
+	
+	// internal register for counting up
+	reg [25:0] count;
+
+	always @(posedge clk) begin
+		if (!resetn)
+			count <= delay;
+		else if (enable) begin
+			if (count == 26'b0)
+				count <= delay;
+			else
+				count <= count - 1'b1;
+		end
+	end
+	
+	// send on signal when it reaches delay
+	assign q = ~{|count};
+
+endmodule // delay_counter
