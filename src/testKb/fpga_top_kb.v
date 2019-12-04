8195c881dd58979c11609db953160d3e2d9279e0
Harry Wang <marvel0usx@outlook.com>
2019-12-04 18:53:39 -0500

    test keyboard

--- src/testKb/fpga_top_kb.v
+++ src/testKb/fpga_top_kb.v
@@ -1,19 +1,28 @@
-module fpga_top_kb(CLOCK_50, KEY, PS2_CLK, PS2_DAT, LEDR, GPIO_0, GPIO_1);
+module fpga_top_kb(CLOCK_50, KEY[0], PS2_CLK, PS2_DAT, LEDR[0]);
     input CLOCK_50;
-    input [3:0] KEY;
+    input [0:0] KEY;
     input PS2_CLK;
     input PS2_DAT;
-    output [9:0] LEDR;
-    inout [35:0] GPIO_0, GPIO_1;
+    output [0:0] LEDR;
 
-    keyboard_adapter kb(
-        .clk(CLOCK_50),
-        .resetn(KEY[0]),
-        .PS2_DAT(PS2_DAT),
+    wire gnd;
+
+    keyboard_tracker #(parameter PULSE_OR_HOLD = 0) t0(
+        .clock(CLOCK_50),
+        .reset(KEY[0]),
         .PS2_CLK(PS2_CLK),
-        .s(LEDR[0]),
-        .GPIO_0(GPIO_0),
-        .GPIO_1(GPIO_1)
+        .PS2_DAT(PS2_DAT),
+        
+        .w(gnd),
+        .a(gnd),
+        .s(gnd),
+        .d(gnd),
+        .left(gnd),
+        .right(gnd),
+        .up(gnd),
+        .down(gnd),
+        .space(LEDR[0]),
+        .enter(gnd)
     );
 
 endmodule // fpga_top_kb
